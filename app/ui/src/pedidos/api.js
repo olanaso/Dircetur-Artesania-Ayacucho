e7bc3fc5115d47bef3e91b1401349a3573be7b54
Max CTR <68170318+max-c-t-r@users.noreply.github.com>
2024-06-11 14:23:15 -0500

    fixed: ultimos cambios arreglados en categorias y pedidos

--- app/ui/src/pedidos/api.js
+++ app/ui/src/pedidos/api.js
@@ -1,22 +1,25 @@
 import { baseUrl} from '../utils/config';
 
-export async function listarPedidos() {
+export async function listarPedidos(page, limit) {
     try {
- const response = await fetch(`${baseUrl}/pedido?page=${page}&limit=${limit}`);
+        const response = await fetch(`${baseUrl}/pedido?page=${page}&limit=${limit}`);
         const result = await response.json();
         return result;
     } catch (error) {
-        console.error("Error al encontrar las categorias:", error);
+        console.error("Error al encontrar las categorías:", error);
+        throw error; // Propagar el error para manejarlo en el front-end si es necesario
     }
 }
 
 export async function filtrarPedidos(filtro) {
     try {
+        // Construir la URL con los parámetros de filtro
         const params = new URLSearchParams(filtro);
         const response = await fetch(`${baseUrl}/pedidos?${params}`);
         const result = await response.json();
         return result;
     } catch (error) {
         console.error("Error al buscar las categorias:", error);
+        throw error; // Propagar el error para manejarlo en el front-end si es necesario
     }
 }
