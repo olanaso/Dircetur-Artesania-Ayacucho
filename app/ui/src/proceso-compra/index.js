3215d3ad58bbdc2fcca384270c95d0303e9bc429
Max CTR <68170318+max-c-t-r@users.noreply.github.com>
2024-08-08 18:48:55 -0500

    fix: campos obligatorios proceso compra

--- app/ui/src/proceso-compra/index.js
+++ app/ui/src/proceso-compra/index.js
@@ -1,10 +1,8 @@
 import '../proceso-compra/style.css';
-
+import { showToast } from '../utils/toast';
 
 // pasos de la compra
 var currentStep = 1;
-
-
 
 function showStep(step) {
     $('.step').removeClass('active');
@@ -13,9 +11,43 @@ function showStep(step) {
     $('.stepper-item').slice(0, step).addClass('active');
 }
 
+function validateStep(step) {
+    var isValid = true;
+
+    // Validar los campos del formulario
+    $('#step' + step + ' .form-control').each(function () {
+        if (!this.checkValidity()) {
+            isValid = false;
+            $(this).addClass('is-invalid');
+        } else {
+            $(this).removeClass('is-invalid');
+        }
+    });
+
+    // Validar el checkbox en el paso 4
+    if (step === 4) {
+        var checkbox = $('#step4Terminos');
+        if (!checkbox.is(':checked')) {
+            isValid = false;
+            checkbox.addClass('is-invalid');
+            showToast("Debe aceptar los términos y condiciones antes de continuar.");
+        } else {
+            checkbox.removeClass('is-invalid');
+        }
+    }
+
+    return isValid;
+}
+
 $('.next').click(function () {
-    currentStep++;
-    showStep(currentStep);
+    if (validateStep(currentStep)) {
+        currentStep++;
+        showStep(currentStep);
+    } else {
+        if (currentStep !== 4) {
+            showToast("Por favor, complete los campos antes de continuar.");
+        }
+    }
 });
 
 $('.prev').click(function () {
@@ -23,3 +55,7 @@ $('.prev').click(function () {
     showStep(currentStep);
 });
 
+// Mostrar el primer paso al cargar la página
+$(document).ready(function () {
+    showStep(currentStep);
+});
