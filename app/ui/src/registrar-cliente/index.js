4c52eaaa5b5be39d22c30e4b9e779db91b507cc8
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-08-08 20:35:32 -0500

    feat(frontend): demo registrar

---
+++ app/ui/src/registrar-cliente/index.js
@@ -0,0 +1,10 @@
+import './flaticon.css'
+import './main.css'
+import './font-awesome.min.css'
+
+async function registrarCliente(e){
+    $('#btnRegistrar').click(async function(e){
+        e.preventDefault()
+
+    }
+}
