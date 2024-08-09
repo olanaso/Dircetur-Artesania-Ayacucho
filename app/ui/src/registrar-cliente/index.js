d55e38c03ffdd704b9ab5126543827e1cfc2d59b
Max CTR <68170318+max-c-t-r@users.noreply.github.com>
2024-08-09 07:53:07 -0500

    diseño interfaz registro

--- app/ui/src/registrar-cliente/index.js
+++ app/ui/src/registrar-cliente/index.js
@@ -1,31 +1,58 @@
 import './flaticon.css'
 import './main.css'
 import './font-awesome.min.css'
-import {showToast} from "../utils/toast.js";
-import {guardarCliente} from './api.js'
-async function registrarCliente(){
-    $(document).ready(function(){
-        $('#btnregistrar').click(async function(e){
-            e.preventDefault()
-            let nombres = $('#nombres').val()
-            let apellidos = $('#apellidos').val()
-            let correo = $('#correo').val()
-            let contraseña = $('#contraseña').val()
-            let telefono = $('#telefono').val()
+import './style.css'
+import { showToast } from "../utils/toast.js";
+import { guardarCliente } from './api.js'
 
-            //usando api
-            try{
-                const registroCliente = await guardarCliente({nombres, apellidos, correo, contraseña, telefono})
-                if(registroCliente)
-                {
-                    showToast('success', 'Cliente registrado correctamente')
-                }
-            }catch(e){
-                showToast('error', 'Error al registrar el cliente') //por cambiar, debe responder de acuerdo al api
-                console.log(e)
-            }
 
-        })
+
+$(document).ready(function () {
+
+
+    $('#btnregistrar').click(async function (e) {
+        e.preventDefault()
+        if (validarCamposRegistro()) {
+            registrarCliente()
+            window.location.href = '#'; 
+        }
     })
+})
+
+function validarCamposRegistro() {
+    let isValid = true;
+    $('#registerForm input[required]').each(function () {
+        if ($(this).val() === '') {
+            isValid = false;
+            $(this).css('border', '1px solid red');
+        } else {
+            $(this).css('border', '');
+        }
+    });
+
+    if (!isValid) {
+        showToast('Por favor, complete todos los campos requeridos.');
+    }
+    return isValid;
+}
+async function registrarCliente() {
+
+    let nombres = $('#nombres').val()
+    let apellidos = $('#apellidos').val()
+    let correo = $('#correo').val()
+    let contraseña = $('#contraseña').val()
+    let telefono = $('#telefono').val()
+
+    //usando api
+    try {
+        const registroCliente = await guardarCliente({ nombres, apellidos, correo, contraseña, telefono })
+        if (registroCliente) {
+            showToast('success', 'Cliente registrado correctamente')
+        }
+    } catch (e) {
+        showToast('error', 'Error al registrar el cliente') //por cambiar, debe responder de acuerdo al api
+        console.log(e)
+    }
 
 }
+
