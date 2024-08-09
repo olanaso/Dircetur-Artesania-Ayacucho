edea844fe8826d6e2e0cdf7ea053996b7e38c167
Max CTR <68170318+max-c-t-r@users.noreply.github.com>
2024-08-09 11:04:31 -0500

    build: navegacion cliente y footer

--- app/ui/src/proceso-compra/index.js
+++ app/ui/src/proceso-compra/index.js
@@ -1,5 +1,40 @@
 import '../proceso-compra/style.css';
 import { showToast } from '../utils/toast';
+import { loadPartials } from '../utils/viewpartials';
+import { hideLoading } from '../utils/init';
+
+
+hideLoading();
+// Uso de la función
+(async function () {
+  let partials = [
+    { path: 'partials/shared/menuNavCliente.html', container: 'main-header' },
+    { path: 'partials/shared/footerCliente.html', container: 'main-footer' },
+
+  ];
+  try {
+    await loadPartials(partials);
+    import('../utils/common')
+
+
+    // Aquí coloca el código que deseas ejecutar después de que todas las vistas parciales se hayan cargado.
+    console.log('Las vistas parciales se han cargado correctamente!');
+    // Por ejemplo, podrías iniciar tu aplicación aquí.
+
+    startApp();
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+function startApp() {
+  setTimeout(function () {
+    //funcion para llenar en otros caoss
+  }, 500);
+}
+
+
+
 
 // pasos de la compra
 var currentStep = 1;
