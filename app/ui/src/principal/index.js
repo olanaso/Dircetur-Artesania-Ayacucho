6befaf5d694f8c2d2d7116b8c5933e6220cd5534
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-08-09 22:12:00 -0500

    feat(frontend): componentes navbar y footer en todos los html

--- app/ui/src/principal/index.js
+++ app/ui/src/principal/index.js
@@ -10,7 +10,37 @@ import './owl-carousel.css'
 import './sliderPro.css'
 import './animated.css'
 import './font-awesome.min.css'
- 
+import {loadPartials} from "../utils/viewpartials.js";
+
+hideLoading();
+// Uso de la función
+(async function () {
+    let partials = [
+        { path: 'partials/shared/menuNavCliente.html', container: 'main-header' },
+        { path: 'partials/shared/footerCliente.html', container: 'main-footer' },
+
+    ];
+    try {
+        await loadPartials(partials);
+        import('../utils/common')
+
+
+        // Aquí coloca el código que deseas ejecutar después de que todas las vistas parciales se hayan cargado.
+        console.log('Las vistas parciales se han cargado correctamente!');
+        // Por ejemplo, podrías iniciar tu aplicación aquí.
+
+        startApp();
+    } catch (e) {
+        console.error(e);
+    }
+})();
+
+function startApp() {
+    setTimeout(function () {
+        //funcion para llenar en otros caoss
+    }, 500);
+}
+
 
 $(document).ready(async function(){
     //await cargarSliders();
--- app/ui/src/proceso-compra/index.js
+++ app/ui/src/proceso-compra/index.js
@@ -4,34 +4,6 @@ import { loadPartials } from '../utils/viewpartials';
 import { hideLoading } from '../utils/init';
 
 
-hideLoading();
-// Uso de la función
-(async function () {
-  let partials = [
-    { path: 'partials/shared/menuNavCliente.html', container: 'main-header' },
-    { path: 'partials/shared/footerCliente.html', container: 'main-footer' },
-
-  ];
-  try {
-    await loadPartials(partials);
-    import('../utils/common')
-
-
-    // Aquí coloca el código que deseas ejecutar después de que todas las vistas parciales se hayan cargado.
-    console.log('Las vistas parciales se han cargado correctamente!');
-    // Por ejemplo, podrías iniciar tu aplicación aquí.
-
-    startApp();
-  } catch (e) {
-    console.error(e);
-  }
-})();
-
-function startApp() {
-  setTimeout(function () {
-    //funcion para llenar en otros caoss
-  }, 500);
-}
 
 
 
