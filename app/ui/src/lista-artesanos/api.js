1096201d22cad6b95e7356d8c02d98aab83d32d0
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-08-18 01:17:47 -0500

    fix(frontend): conexion con el api

--- app/ui/src/lista-artesanos/api.js
+++ app/ui/src/lista-artesanos/api.js
@@ -3,21 +3,12 @@ import { baseUrl } from '../utils/config';
 
 export async function listarArtesanoById(idArtesano) {
     try {
-        const response = await fetch(`${baseUrl}/artesano/${idArtesano}`, {
-            method: 'GET',
-        });
+        const requestOptions ={
+            method: 'GET'
+        }
+        const response = await fetch(`${baseUrl}/v1/artesanos/categorias}`, requestOptions);
         const result = await response.json();
-        return result.data; // Return the data property from the response
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-export async function obtenerProducByArtesano(idArtesano) {
-    try {
-        const response = await fetch(`${baseUrl}/v1/productos/artesanos/${idArtesano}`, {
-            method: 'GET',
-        });
-        const result = await response.json();
+        console.log("DATAAA", result)
         return result.data; // Return the data property from the response
     } catch (error) {
         console.error('Error:', error);
