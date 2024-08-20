b902752bdd4234c85fac630c8085f4db585cb0b4
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-08-20 10:45:09 -0500

    feat(backend): productos desados get

--- app/server/models/productos_favoritos.js
+++ app/server/models/productos_favoritos.js
@@ -1,5 +1,8 @@
 const db = require('../config/db');
 const {DataTypes} = require('sequelize');
+const cliente = require('./cliente')
+const producto = require('./producto')
+const artesano = require('./artesano')
 sequelize = db.sequelize;
 
 const productosFavoritos = sequelize.define('productos_favoritos',
@@ -21,5 +24,37 @@ const productosFavoritos = sequelize.define('productos_favoritos',
         tableName: 'productos_favoritos',
 
 })
+
+productosFavoritos.belongsTo(cliente, {
+    foreignKey: 'id_cliente',
+    as: 'datosCliente'
+})
+
+productosFavoritos.belongsTo(producto, {
+    foreignKey: 'id_producto',
+    as: 'datosProducto'
+})
+
+productosFavoritos.findAllWishProductsByClientId = async (idCliente) => {
+    return await productosFavoritos.findAll(
+        {where : {id_cliente: idCliente},
+            attributes: ['id_producto', 'id_cliente'],
+        include: [
+            {
+                model: producto,
+                as: 'datosProducto',
+                attributes: ['artesano_id', 'nombres_es', 'precio', 'imagen_principal', 'lst_imagenes'],
+                include: [
+                    {
+                        model: artesano,
+                        as: 'datos_artesano',
+                        attributes: ['nombres', 'apellidos']
+                    }
+                ]
+            }
+        ]
+    })
+}
+
 
 module.exports = productosFavoritos
