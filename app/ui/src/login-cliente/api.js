93011fcef6ceb7009a7d095358588874a23e8e19
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-08-21 01:39:15 -0500

    fix(backend): login funcionando con nuevo endpoint

--- app/ui/src/login-cliente/api.js
+++ app/ui/src/login-cliente/api.js
@@ -11,14 +11,20 @@ export async function loginCliente(usuario,clave){
         }),
     }
     try{
-        const response = await fetch(baseUrl + "/login", settings)
+        const response = await fetch(baseUrl + "/v1/login-clientes", settings)
         const data = await response.json()
+        // console.log("La dataaa", typeof data)
+        // console.log("La dataaa", data)
+        // console.log("La dataaa", data.data.token)
+        // console.log("La dataaa", data.data.id)
+        // console.log("La dataaa", data.data.idCliente)
         //almaceno la data importante en local storage
-        if(data && data.token){
+        if(data && data.data.token){
             saveDataToLocalStorage('rol', 3)
-            saveDataToLocalStorage('token', data.token)
-            saveDataToLocalStorage('id', id)
-            saveDataToLocalStorage('idCliente', data.idCliente)
+            saveDataToLocalStorage('token', data.data.token)
+            saveDataToLocalStorage('id', data.data.id)
+            saveDataToLocalStorage('idCliente', data.data.idCliente)
+
         }
         return data
     } catch(e){
