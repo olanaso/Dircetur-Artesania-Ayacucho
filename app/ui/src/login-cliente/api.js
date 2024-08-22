10d49c16fdb6e701af7a07dda970cfb19303c0c8
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-08-22 12:14:33 -0500

    feat(frontend): reenvio a nueva pantalla

--- app/ui/src/login-cliente/api.js
+++ app/ui/src/login-cliente/api.js
@@ -12,21 +12,7 @@ export async function loginCliente(usuario,clave){
     }
     try{
         const response = await fetch(baseUrl + "/v1/login-clientes", settings)
-        const data = await response.json()
-        // console.log("La dataaa", typeof data)
-        // console.log("La dataaa", data)
-        // console.log("La dataaa", data.data.token)
-        // console.log("La dataaa", data.data.id)
-        // console.log("La dataaa", data.data.idCliente)
-        //almaceno la data importante en local storage
-        if(data && data.data.token){
-            saveDataToLocalStorage('rol', 3)
-            saveDataToLocalStorage('token', data.data.token)
-            saveDataToLocalStorage('id', data.data.id)
-            saveDataToLocalStorage('idCliente', data.data.idCliente)
-
-        }
-        return data
+        return response //devuelvo la respuesta (status, body, headers, etc)
     } catch(e){
         console.error('Error:', e)
     }
