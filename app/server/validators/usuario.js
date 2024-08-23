e1282bfa75be141bc9896417c485bc90ac52aefc
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-08-23 13:11:34 -0500

    fix(frontend): validacion y endpoint para cambiar contrasenia de cliente creado

--- app/server/validators/usuario.js
+++ app/server/validators/usuario.js
@@ -2,13 +2,35 @@ const {check} = require('express-validator')
 const {validateResults} = require('../utils/handleValidator')
 const usuario = require('../models/usuario')
 
-// const validatorUpdateUsuario = [
-//     check('nombre_completo').custom(async nombre_completo =>{
-//         const usuarioEncontrado = await usuario.findOne({where: {nombre_completo}})
-//         if(!usuarioEncontrado){
-//
-//         }
-//
-//     })
-//
-// ]
+const validatorUpdatePasswordCliente = [
+    //validacion de contrasenia actual
+    check('clave').custom(async(clave, {req})=>{
+        const{id} = req.params
+        const usuarioEncontrado = await usuario.findUsuarioById(id)
+        if(clave !== usuarioEncontrado.clave){
+            throw new Error("La contraña no coincide con la actual")
+        }
+    }),
+    //validacion de existencia de usuario
+    check().custom(async(_,{req}) =>{
+        const{id} = req.params
+        const usuarioEncontrado = await usuario.findUsuarioById(id)
+        if(!usuarioEncontrado){
+            throw new Error("El usuario no existe")
+        }
+    }),
+    //validacion de contrasenia nueva
+    check('contraseniaNueva').notEmpty().withMessage('El campo de contraseña nueva es obligatorio'),
+    check('contraseniaConfirmacion').notEmpty().withMessage('El campo de confirmación es obligatorio'),
+    check('contraseniaNueva','contraseniaConfirmacion').custom(async(contraseniaNueva, contraseniaConfirmacion) =>{
+        if(contraseniaNueva !== contraseniaConfirmacion){
+            throw new Error("Las contrasenias no coinciden")
+        }
+    }),
+    (req, res, next) => {
+        return validateResults(req,res,next)
+    }
+
+]
+
+module.exports = {validatorUpdatePasswordCliente}
