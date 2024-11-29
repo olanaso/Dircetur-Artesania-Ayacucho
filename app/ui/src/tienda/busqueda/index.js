8613065b5af72a08af036a53789f98812812fb61
Erick Simón Escalante Olano <ericks.escalanteolano@gmail.com>
2024-11-29 01:16:26 -0500

    busqueda de artesania

--- app/ui/src/tienda/busqueda/index.js
+++ app/ui/src/tienda/busqueda/index.js
@@ -172,78 +172,40 @@ function loadProductos (data) {
         }
         //alert(item.imagen)
         html = html + `
-         <div class="col-6 col-md-4 mb-4">
-            <div class="car-item wow fadeIn" data-wow-duration="0.75s">
-                <div class="thumb-content">
-                    <div class="car-banner nuevo-producto"></div>
-                    <div class="thumb-inner photo-prod">
-                        <a href="producto.html?producto=${encodeURIComponent(JSON.stringify(artenia_deseados))}">
-                            <img style="height:250px; width:100%; object-fit: cover;" 
-                                 src="${item?.imagen_principal || 'https://via.placeholder.com/400x200'}" 
-                                 alt="Producto">
-                        </a>
-                    </div>
-                </div>
-     <div class="down-content text-center" style="max-width: 300px;">
-    <a href="producto.html?producto=${encodeURIComponent(JSON.stringify(artenia_deseados))}" style="color:#000; word-wrap: break-word; overflow-wrap: break-word;">
-        <h5 title="${item?.nombres_es || ''}" class="card-title font-weight-bold product-description" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; display: block; max-width: 100%; margin-top: 20px;">
-            <span class="d-block d-md-none" style="font-size: 1rem; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 100%;">${item?.nombres_es || ''}</span>
-            <span class="d-none d-md-block" style="font-size: 16px; color:#000; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 100%;">${item?.nombres_es || ''}</span>
-        </h5>
-    </a>
-                    <p class="text-danger font-weight-bold">
-                        <span class="d-block d-md-none h5"  style="color:#ea0397">${formatearNumero(item?.precio) || ''} PEN</span>
-                        <span class="d-none d-md-block h4"  style="color:#ea0397">${formatearNumero(item?.precio) || ''} PEN</span>
-                    </p>
-                    <p class="card-text text-muted">
-                        <span class="d-block d-md-none" style="font-size: 12px; color:black"> ${item?.categoria || ''}</span>
-                        <span class="d-none d-md-block" style="font-size: 12px; color:black"> ${item?.categoria || ''}</span>
-                    </p>
-                    <div class="d-flex align-items-center mt-3" title="Artesano">
-                        <img class="rounded-circle mr-2"
-                             src="${item?.foto1 || 'https://via.placeholder.com/40'}"
-                             alt="Foto del artesano" 
-                             style="width: 40px; height: 40px;">
-                        <span class="text-dark" style="margin: 0; padding: 0; display: inline-block;">
-                            <a style="color: black; font-size: 10px; margin: 0; padding: 0; line-height: 1;" 
-                               href="artesano.html?id=${item?.artesano_id || ''}">
-                               ${item?.artesano || ''}
-                            </a>
-                        </span>
-                        <div class="line-dec2"></div>
-                    </div>
-                    <div class="d-flex mt-4 justify-content-center ">
-                        <div class="hidden sm-block btn-group" role="group" aria-label="Basic example">
-                            <button type="button" class="btn btn-light" style="background:#ea0397 !important">
-                                <a style="color:#fff !important" href="producto.html?producto=${encodeURIComponent(JSON.stringify(artenia_deseados))}">Ver más</a>
-                            </button>
-                            <button title="Agregar carrito de compras" type="button" class="btn btn-light btn-comprar" producto_id="${item.id}">
-                            <a title="Agregar carrito de compras" href="carrito-de-compra.html?producto=${encodeURIComponent(JSON.stringify(artenia_anviar_carrito))}">
-                       
-                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                     <path d="M0 0.5C0 0.367392 0.0526784 0.240215 0.146447 0.146447C0.240215 0.0526784 0.367392 0 0.5 0H2C2.11153 3.08115e-05 2.21985 0.0373507 2.30773 0.106025C2.39561 0.174699 2.45801 0.270784 2.485 0.379L2.89 2H14.5C14.5734 2.00007 14.6459 2.0163 14.7124 2.04755C14.7788 2.0788 14.8375 2.12429 14.8844 2.1808C14.9313 2.23731 14.9651 2.30345 14.9835 2.37452C15.002 2.44558 15.0045 2.51984 14.991 2.592L13.491 10.592C13.4696 10.7066 13.4087 10.8101 13.3191 10.8846C13.2294 10.9591 13.1166 10.9999 13 11H4C3.88343 10.9999 3.77057 10.9591 3.68091 10.8846C3.59126 10.8101 3.53045 10.7066 3.509 10.592L2.01 2.607L1.61 1H0.5C0.367392 1 0.240215 0.947322 0.146447 0.853553C0.0526784 0.759785 0 0.632608 0 0.5ZM5 11C4.46957 11 3.96086 11.2107 3.58579 11.5858C3.21071 11.9609 3 12.4696 3 13C3 13.5304 3.21071 14.0391 3.58579 14.4142C3.96086 14.7893 4.46957 15 5 15C5.53043 15 6.03914 14.7893 6.41421 14.4142C6.78929 14.0391 7 13.5304 7 13C7 12.4696 6.78929 11.9609 6.41421 11.5858C6.03914 11.2107 5.53043 11 5 11ZM12 11C11.4696 11 10.9609 11.2107 10.5858 11.5858C10.2107 11.9609 10 12.4696 10 13C10 13.5304 10.2107 14.0391 10.5858 14.4142C10.9609 14.7893 11.4696 15 12 15C12.5304 15 13.0391 14.7893 13.4142 14.4142C13.7893 14.0391 14 13.5304 14 13C14 12.4696 13.7893 11.9609 13.4142 11.5858C13.0391 11.2107 12.5304 11 12 11ZM5 12C5.26522 12 5.51957 12.1054 5.70711 12.2929C5.89464 12.4804 6 12.7348 6 13C6 13.2652 5.89464 13.5196 5.70711 13.7071C5.51957 13.8946 5.26522 14 5 14C4.73478 14 4.48043 13.8946 4.29289 13.7071C4.10536 13.5196 4 13.2652 4 13C4 12.7348 4.10536 12.4804 4.29289 12.2929C4.48043 12.1054 4.73478 12 5 12ZM12 12C12.2652 12 12.5196 12.1054 12.7071 12.2929C12.8946 12.4804 13 12.7348 13 13C13 13.2652 12.8946 13.5196 12.7071 13.7071C12.5196 13.8946 12.2652 14 12 14C11.7348 14 11.4804 13.8946 11.2929 13.7071C11.1054 13.5196 11 13.2652 11 13C11 12.7348 11.1054 12.4804 11.2929 12.2929C11.4804 12.1054 11.7348 12 12 12Z" fill="black"/>
-                                </svg> 
-                                    
-                            </a>
-                            </button>
 
-                            
-                            <button title="Agregar mis favoritos" type="button" class="btn btn-light btn-favoritos" producto_id="${item.id}">
- <a href="productos-deseados.html?producto=${encodeURIComponent(JSON.stringify(artenia_deseados))}">
-
-  <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7.5 13.7625L6.4125 12.7725C2.55 9.27 0 6.9525 0 4.125C0 1.8075 1.815 0 4.125 0C5.43 0 6.6825 0.6075 7.5 1.56C8.3175 0.6075 9.57 0 10.875 0C13.185 0 15 1.8075 15 4.125C15 6.9525 12.45 9.27 8.5875 12.7725L7.5 13.7625Z" fill="red"/>
-                                </svg> 
-                                
-   </a>
-                               
-                            </button>
+                  <div class="col-md-4" >
+                <div class="card__busqueda shadow-lg">
+                 <a href="producto.html?producto=${encodeURIComponent(JSON.stringify(artenia_deseados))}">
+                    <img class="card__busqueda-img-top" style="height:250px; width:100%; object-fit: cover;  transform: scale(0.8);  transform-origin: center;  "   src="${item?.imagen_principal || 'https://via.placeholder.com/300x250'}" alt="Casaca artesanal">
+                    </a>
+                    <div class="card-body text-center">
+                        <h5 class="card__busqueda-title">${item?.nombres_es || ''}</h5>
+                        <p class="card__busqueda-price">${formatearNumero(item?.precio) || ''} PEN</p>
+                        <p class="card__busqueda-category"> ${item?.categoria || ''}</p>
+                        <div class="d-flex align-items-center justify-content-center my-3">
+                         <a  href="artesano.html?id=${item?.artesano_id || ''}"> 
+                            <img  src="${item?.foto1 || 'https://via.placeholder.com/40'}" alt="Artesano" class="card__busqueda-artisan-img"></a>
+                            <div class="ml-3 text-left">
+                               <a  href="artesano.html?id=${item?.artesano_id || ''}"> <p class="card__busqueda-artisan mb-0">
+                              ${item?.artesano || ''}
+                               </p></a>
+                                <p class="card__busqueda-location mb-0">Artesano</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between card__busqueda-buttons">
+                            <a href="producto.html?producto=${encodeURIComponent(JSON.stringify(artenia_deseados))}" class="btn btn-primary btn-sm">Ver más</a>
+                            <a title="Agregar carrito de compras" class="btn btn-outline-dark btn-sm" href="carrito-de-compra.html?producto=${encodeURIComponent(JSON.stringify(artenia_anviar_carrito))}">
+                                <i class="fas fa-shopping-cart"></i>
+                            </a>
+                            <a  title="Agregar mis favoritos" class="btn btn-outline-danger btn-sm" href="productos-deseados.html?producto=${encodeURIComponent(JSON.stringify(artenia_deseados))}">
+                                <i class="fas fa-heart"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            
+     
         `
 
     }
