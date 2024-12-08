8eabeb17620e5df672864709430cb26a40efde80
Jhampier Tucno <yantucnoperez@gmail.com>
2024-12-07 23:39:56 -0500

    feat: implement multiple currency conversion methods with fallback mechanism

--- app/server/utils/convertirMoneda.js
+++ app/server/utils/convertirMoneda.js
@@ -1,14 +1,13 @@
 
 // Parámetros de la API
-const access_key = 'ad3f75eb3ea982e01dcfcffa'; // Reemplázalo con tu clave de API
+const access_key = 'a5fb231caad198f0601087f7'; // Reemplázalo con tu clave de API
 
 // Función para realizar la conversión de moneda
-const convertirMoneda = async (from, to, amount) => {
+const convertirMonedaKambista = async (from, to, amount) => {
     try {
         // Construir la URL de la API
-        // const url = `https://api.exchangerate.host/${endpoint}?access_key=${access_key}&from=${from}&to=${to}&amount=${amount}`;
-        // const url = `https://v6.exchangerate-api.com/v6/${access_key}/latest/${from}`;
-        const url = 'https://api.kambista.com/v1/exchange/calculates?originCurrency=USD&destinationCurrency=PEN&amount=1500&active=S'
+        const url = `https://api.kambista.com/v1/exchange/calculates?originCurrency=${from}&destinationCurrency=${to}&amount=${amount}&active=S`
+        
         // Hacer la petición GET usando fetch
         const response = await fetch(url);
 
@@ -25,17 +24,60 @@ const convertirMoneda = async (from, to, amount) => {
             throw new Error(data["error-type"]);
         }
 
-        const conversionRate = 1 / (data.tc.ask || 3.8);
-
         // Obtener el resultado de la conversión
-        const conversionResult = (amount * conversionRate).toFixed(2);
-        console.log(`Resultado de la conversión: ${amount} ${from} = ${conversionResult} ${to}`);
+        const conversionResult = data.exchange.toFixed(2);
 
         return conversionResult;
     } catch (error) {
-        console.error('Error al realizar la conversión:', error.message);
+        console.error('Error al realizar la conversión convertirMonedaKambista:', error.message);
     }
 }
+
+const convertirMonedaExchangerate = async (from, to, amount) => {
+    try {
+        // Construir la URL de la API
+        const url = `https://v6.exchangerate-api.com/v6/${access_key}/latest/${from}`;
+
+        // Hacer la petición GET usando fetch
+        const response = await fetch(url);
+
+        // Comprobar si la respuesta es exitosa
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.status}`);
+        }
+
+        // Convertir la respuesta a JSON
+        const data = await response.json();
+
+        // Verificar si la API devolvió un error
+        if (data.result === "error") {
+            throw new Error(data["error-type"]);
+        }
+
+        const conversionRate = data.conversion_rates[to];
+
+        // Obtener el resultado de la conversión
+        const conversionResult = (amount * conversionRate).toFixed(2);
+
+        return conversionResult;
+    } catch (error) {
+        console.error('Error al realizar la conversión convertirMonedaExchangerate:', error.message);
+    }
+}
+
+const convertirMoneda = async (from, to, amount) => {
+    const exchanges = [convertirMonedaKambista, convertirMonedaExchangerate];
+
+    for (let exchange of exchanges) {
+        const result = await exchange(from, to, amount);
+        if (result) {
+            return result;
+        }
+    }
+
+    return null;
+}
+
 
 module.exports = { convertirMoneda };
 
