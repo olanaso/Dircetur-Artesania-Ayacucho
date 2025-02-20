bc2b8fa75aa14695950a31b1f89f87b9934161a2
Jhampier Tucno <yantucnoperez@gmail.com>
2025-02-19 19:33:17 -0500

    refactor: improve header layout and enhance search functionality in header.html; update API URLs in config.js

--- app/ui/src/utils/config.js
+++ app/ui/src/utils/config.js
@@ -1,22 +1,22 @@
-
-//export const baseUrl = 'https://api-certificados.centrodegestion.org'; 
+//export const baseUrl = 'https://api-certificados.centrodegestion.org';
 // export const baseUrl = 'http://148.113.196.34:80/api';
 
-export const baseUrl = 'http://localhost:3002/api';
-// export const baseUrl = 'https://app.artesaniasdeayacucho.pe/api';
+// export const baseUrl = 'http://localhost:3002/api';
+export const baseUrl = "https://app.artesaniasdeayacucho.pe/api";
 
-export const urlSeguimientoPedido = 'http://localhost:4200/';
-// export const urlSeguimientoPedido = 'https://seguimiento.artesaniasdeayacucho.pe/';
+// export const urlSeguimientoPedido = 'http://localhost:4200/';
+export const urlSeguimientoPedido =
+  "https://seguimiento.artesaniasdeayacucho.pe/";
 
 //export const baseUrl = 'https://fantastic-zebra-65wpjr4xvjh5xgv-3002.app.github.dev/api';
-export const baseUrldni = 'https://dni.biblio-ideas.com/api';
+export const baseUrldni = "https://dni.biblio-ideas.com/api";
 /*Seccion de la llamda al wordpress*/
-export const URL_GLOBAL = 'https://blog.artesaniasdeayacucho.pe/';
+export const URL_GLOBAL = "https://blog.artesaniasdeayacucho.pe/";
 export const categoryNews = 4;
 export const sizeNews = 10;
 
 //} Function to save data in localStorage
-export function saveDataToLocalStorage (key, data) {
+export function saveDataToLocalStorage(key, data) {
   try {
     // Convert the data to a JSON string
     const dataJSON = JSON.stringify(data);
@@ -24,26 +24,26 @@ export function saveDataToLocalStorage (key, data) {
     localStorage.setItem(key, dataJSON);
     console.log(`Data with key "${key}" saved to localStorage.`);
   } catch (error) {
-    console.error('Error while saving data to localStorage:', error);
+    console.error("Error while saving data to localStorage:", error);
   }
 }
 
-
-export function getBasePathWithPort () {
+export function getBasePathWithPort() {
   const parsedUrl = new URL(baseUrl);
   // Si no hay un puerto explícito, utiliza el puerto por defecto según el protocolo
-  const port = parsedUrl.port || (parsedUrl.protocol === 'https:' ? '443' : '80');
+  const port =
+    parsedUrl.port || (parsedUrl.protocol === "https:" ? "443" : "80");
   return `${parsedUrl.protocol}//${parsedUrl.hostname}:${port}`;
 }
 
-export function getBaseUrl (url) {
+export function getBaseUrl(url) {
   const urlObj = new URL(url);
   return `${urlObj.protocol}//${urlObj.hostname}:${urlObj.port}`;
 }
 
 // Function to get data from localStorage
-export function getDataFromLocalStorage (key) {
-  return JSON.parse(localStorage.getItem(key))
+export function getDataFromLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
   // try {
   //   // Get the data from localStorage using the given key
   //   const dataJSON = localStorage.getItem(key);
